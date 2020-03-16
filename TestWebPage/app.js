4a22e35f07376683ba0ffbfe884e8b929eeb8c83
tanaka.kenji <tanaka.kenji@synamon.jp>
2020-03-16 22:51:53 +0900

    TestWebPage Bug Fix

--- TestWebPage/app.js
+++ TestWebPage/app.js
@@ -3,20 +3,24 @@ ws.onopen = evt => {
     console.log('ws open');
 };
 ws.onmessage = async evt => {
-    console.log(`${evt.data}`);
     const msg = JSON.parse(evt.data);
+    console.log(msg.type);
     if (msg.sdp) {
+        console.log(`set remote offer: ${evt.data}`);
         await pc.setRemoteDescription(msg);
         if (msg.type === 'offer') {
             const answer = await pc.createAnswer();
             await pc.setLocalDescription(answer);
-            ws.send(JSON.stringify(answer));
-        } else if (msg.type === 'candidate') {
-            try{
-                await pc.addIceCandidate(msg);
-            }catch(ex) {
-                console.log(ex.message);
-            }
+            const sendData = JSON.stringify(answer);
+            console.log(`send answer: ${sendData}`);
+            ws.send(sendData);
+        }
+    } else if (msg.type === 'candidate') {
+        try {
+            console.log(`add ice candidate: ${evt.data}`);
+            await pc.addIceCandidate(msg);
+        } catch (ex) {
+            console.log(ex.message);
         }
     }
 };
@@ -30,8 +34,11 @@ ws.onerror = _ => {
 const pc = new RTCPeerConnection({ iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] });
 pc.onicecandidate = evt => {
     console.log(`onicecandidate: ${evt.candidate}`);
-    if (evt.candidate)
-        ws.send(JSON.stringify({ type: 'candidate', candidate: evt.candidate.candidate, sdpMid: evt.candidate.sdpMid, sdpMLineIndex: evt.candidate.sdpMLineIndex }));
+    if (evt.candidate) {
+        const sendData = JSON.stringify({ type: 'candidate', candidate: evt.candidate.candidate, sdpMid: evt.candidate.sdpMid, sdpMLineIndex: evt.candidate.sdpMLineIndex });
+        console.log(`send candidate: ${sendData}`);
+        ws.send(sendData);
+    }
 };
 pc.ontrack = evt => {
     console.log(`ontrack: ${evt.track.kind}`);
